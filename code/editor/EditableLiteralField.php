9887201bf59573c812492087473043f5957a9a61
Will Rossiter <will@silverstripe.com>
2009-03-25 03:37:49 +0000

    FEATURE: added LiteralField option to user defined forms

---
+++ code/editor/EditableLiteralField.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Editable Spam Protecter Field. Used with the User Defined Forms module (if 
+ * installed) to allow the user to have captcha fields with their custom forms
+ * 
+ * @package SpamProtection
+ */
+
+class EditableLiteralField extends EditableFormField {
+	
+	static $db = array(
+		'Content' => 'Text'
+	);
+	
+	static $singular_name = 'Literal Field';
+	static $plural_name = 'Literal Fields';
+	
+	function __construct( $record = null, $isSingleton = false ) {
+
+		parent::__construct( $record, $isSingleton );
+	}
+	
+	function ExtraOptions() {
+		
+		// eventually replace hard-coded "Fields"?
+		$baseName = "Fields[$this->ID]";
+		
+		$extraFields = new FieldSet();
+		$extraFields->push(new TextareaField($baseName . "[Content]", "Text", 4, 20, $this->Content));
+		
+		return $extraFields;		
+	}
+	
+	function populateFromPostData($data) {
+
+		$this->Content = $data['Content'];
+		parent::populateFromPostData($data);
+	}
+	
+	function getFormField() {
+		return $this->createField();
+	}
+	
+	function getFilterField() {
+		return $this->createField(true);
+	}
+	
+	function createField() {
+		return new LiteralField("LiteralField[$this->ID]", 
+			"<label class='left'>$this->Name</label><div class='middleColumn literalFieldArea'>". $this->Content."</div>");
+	}
+	/**
+	 * Populates the default fields. 
+	 */
+	function DefaultField() {
+		return "";
+	}
+	
+	function EditSegment() {
+		return $this->renderWith( $this->class );
+	}
+}
+?>
