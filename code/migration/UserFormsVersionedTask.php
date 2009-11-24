85c30f68e4eebc91dac3663f626f5bcc53d7b9ae
Will Rossiter <will@silverstripe.com>
2009-11-24 21:07:36 +0000

    FEATURE: added migration tool for publishing unversioned forms

---
+++ code/migration/UserFormsVersionedTask.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * UserForms Versioned Task
+ *
+ * Initial migration script for forms that do not exist on the live site.
+ * In previous versions of UserForms it did not provide staging / live functionality
+ * When upgrading to the new version we need to publish the existing pages.
+ *
+ * @package userforms
+ */
+
+class UserFormsVersionedTask extends MigrationTask {
+
+	protected $title = "UserForms Versioned Initial Migration";
+
+	protected $description = "Publishes the existing forms";
+	
+	/**
+	 * Publish the existing forms.
+	 *
+	 */
+	function run($request) {
+		$forms = Versioned::get_by_stage('UserDefinedForm', 'Live');
+		
+		if($forms) {
+			foreach($forms as $form) {
+				echo "Publishing $form->Title <br />";
+				$form->doPublish();
+			}
+			echo "Complete";
+		}
+		else {
+			echo "No Forms Found";
+		}
+	}
+}
+
+
