a81a164bcfb549c3dbe8c99fe427e0f4e6291076
Will Rossiter <will.rossiter@gmail.com>
2011-12-05 21:38:10 +1300

    FEATURE: allow EditableLiteralFields to be hidden from report extracts. Fixes http://open.silverstripe.org/ticket/6704

--- code/editor/EditableLiteralField.php
+++ code/editor/EditableLiteralField.php
@@ -15,12 +15,32 @@ class EditableLiteralField extends EditableFormField {
 	
 	function getFieldConfiguration() {
 		return new FieldSet(
-			new TextareaField("Fields[$this->ID][CustomSettings][Content]", "HTML", 4, 20, $this->getSetting('Content'))
+			new TextareaField(
+				"Fields[$this->ID][CustomSettings][Content]", 
+				"HTML", 4, 20, $this->getSetting('Content')
+			),
+			new CheckboxField(
+				"Fields[$this->ID][CustomSettings][HideFromReports]", 
+				_t('EditableLiteralField.HIDEFROMREPORT', 'Hide from reports?'), 
+				$this->getSetting('HideFromReports')
+			)
 		);
 	}
 
 	function getFormField() {
 		return new LiteralField("LiteralField[$this->ID]", 
-			"<div id='$this->Name' class='field text'><label class='left'>$this->Title</label><div class='middleColumn literalFieldArea'>". $this->getSetting('Content') ."</div></div>");
+			"<div id='$this->Name' class='field text'>
+				<label class='left'>$this->Title</label>
+				<div class='middleColumn literalFieldArea'>". $this->getSetting('Content') ."</div>".
+			"</div>"
+		);
+	}
+	
+	function showInReports() {
+		if($this->getSetting('HideFromReports')) {
+			return false;
+		}
+		
+		return true;
 	}
 }
