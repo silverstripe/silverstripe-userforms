784d20e151b3ffa46ec67e0a4668b44373e2b23e
Will Rossiter <will.rossiter@gmail.com>
2013-07-15 22:05:45 +1200

    Update javascript path. (Fixes #126)

--- _config.php
+++ _config.php
@@ -1,6 +1,4 @@
 <?php
 
-LeftAndMain::require_javascript('userforms/javascript/FieldEditor.js');
+LeftAndMain::require_javascript('userforms/javascript/UserForm.js');
 LeftAndMain::require_css('userforms/css/FieldEditor.css');
-
-?>
