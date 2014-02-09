938d2059fac6ed0bff0c54094c96249126221eed
Kirk Mayo <kirk@silverstripe.com>
2014-02-10 12:40:20 +1300

    NEW: Added a numeric field and associated png
    NEW: Setup translations for number field
    
    NEW: extend fromEditableTextfield use number class

---
+++ code/model/formfields/EditableNumericField.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * EditableNumericField
+ *
+ * This control represents a user-defined numeric field in a user defined form
+ *
+ * @package userforms
+ */
+
+class EditableNumericField extends EditableTextField {
+
+	private static $singular_name = 'Numeric Field';
+	
+	private static $plural_name = 'Numeric Fields';
+	
+
+	/**
+	 * @return TextareaField|TextField
+	 */
+	public function getFormField() {
+		if($this->getSetting('Rows') && $this->getSetting('Rows') > 1) {
+			$taf = new NumericField($this->Name, $this->Title);
+			$taf->setRows($this->getSetting('Rows'));
+			$taf->addExtraClass('number');
+			return $taf;
+		}
+		else {
+			$taf = new NumericField($this->Name, $this->Title, null, $this->getSetting('MaxLength'));
+			$taf->addExtraClass('number');
+                        return $taf;
+
+		}
+	}
+}
