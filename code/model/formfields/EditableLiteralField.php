68b29e13e2e308296a14936e8162211a681da1cf
Damian Mooyman <damian@silverstripe.com>
2015-02-10 15:32:44 +1300

    BUG Use correct api for assigning field value BUG Fix encoding of cms add-option in js ENHANCEMENT Sanitise literal HTML content

--- code/model/formfields/EditableLiteralField.php
+++ code/model/formfields/EditableLiteralField.php
@@ -12,14 +12,70 @@ class EditableLiteralField extends EditableFormField {
 	private static $singular_name = 'HTML Block';
 	
 	private static $plural_name = 'HTML Blocks';
+
+	/**
+	 * Get the name of the editor config to use for HTML sanitisation. Defaults to the active config.
+	 *
+	 * @var string
+	 * @config
+	 */
+	private static $editor_config = null;
+
+	/**
+	 * Returns the {@see HtmlEditorConfig} instance to use for sanitisation
+	 *
+	 * @return HtmlEditorConfig
+	 */
+	protected function getEditorConfig() {
+		$editorConfig = $this->config()->editor_config;
+		if($editorConfig) return HtmlEditorConfig::get($editorConfig);
+		return HtmlEditorConfig::get_active();
+	}
+
+	/**
+	 * Safely sanitise html content, if enabled
+	 *
+	 * @param string $content Raw html
+	 * @return string Safely sanitised html
+	 */
+	protected function sanitiseContent($content) {
+		// Check if sanitisation is enabled
+		if(!HtmlEditorField::config()->sanitise_server_side) return $content;
+
+		// Perform sanitisation
+		$htmlValue = Injector::inst()->create('HTMLValue', $content);
+		$santiser = Injector::inst()->create('HtmlEditorSanitiser', $this->getEditorConfig());
+		$santiser->sanitise($htmlValue);
+		return $htmlValue->getContent();
+	}
+
+	/**
+	 * Get HTML Content of this literal field
+	 *
+	 * @return string
+	 */
+	public function getContent() {
+		// Apply html editor sanitisation rules
+		$content = $this->getSetting('Content');
+		return $this->sanitiseContent($content);
+	}
+
+	/**
+	 * Set the content with the given value
+	 *
+	 * @param string $content
+	 */
+	public function setContent($content) {
+		// Apply html editor sanitisation rules
+		$content = $this->sanitiseContent($content);
+		$this->setSetting('Content', $content);
+	}
 	
 	public function getFieldConfiguration() {
-		$customSettings = unserialize($this->CustomSettings);	
-		$content = (isset($customSettings['Content'])) ? $customSettings['Content'] : '';
 		$textAreaField = new TextareaField(
 			$this->getSettingName('Content'),
 			"HTML",
-			$content
+			$this->getContent()
 		);
 		$textAreaField->setRows(4);
 		$textAreaField->setColumns(20);
