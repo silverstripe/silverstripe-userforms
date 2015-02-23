83c2774c454174c98b519e6d5f9e51d9e3605753
Damian Mooyman <damian@silverstripe.com>
2015-02-23 16:54:31 +1300

    Update translations

---
+++ javascript/lang/src/eo.js
@@ -0,0 +1,14 @@
+{
+    "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "Ĉiuj sendaĵoj malaperos porĉiame. Ĉu daŭrigu?",
+    "UserForms.ERROR_CREATING_FIELD": "Eraris kreante kampon",
+    "UserForms.ADDING_FIELD": "Aldonas novan kampon",
+    "UserForms.ADDED_FIELD": "Aldonis novan kampon",
+    "UserForms.HIDE_OPTIONS": "Kaŝi agordojn",
+    "UserForms.SHOW_OPTIONS": "Vidigi agordojn",
+    "UserForms.ADDING_OPTION": "Aldonas agordon",
+    "UserForms.ADDED_OPTION": "Aldonis agordon",
+    "UserForms.ERROR_CREATING_OPTION": "Eraris kreante agordon",
+    "UserForms.REMOVED_OPTION": "Forigis agordon",
+    "UserForms.ADDING_RULE": "Aldonas regulon",
+    "GRIDFIELD.ERRORINTRANSACTION": "Eraris venigante datumojn el la servilo.\nBonvolu reprovi poste."
+}
