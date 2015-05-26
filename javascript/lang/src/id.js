e9df40cc5a8a8f0a5c9b987158b3ba96f3cd7417
Damian Mooyman <damian@silverstripe.com>
2015-05-26 18:20:25 +1200

    Update translations

---
+++ javascript/lang/src/id.js
@@ -0,0 +1,14 @@
+{
+    "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "Semua pengiriman akan dihapus secara permanen. Lanjutkan?",
+    "UserForms.ERROR_CREATING_FIELD": "Error creating field",
+    "UserForms.ADDING_FIELD": "Adding new field",
+    "UserForms.ADDED_FIELD": "Added new field",
+    "UserForms.HIDE_OPTIONS": "Sembunyikan Pilihan",
+    "UserForms.SHOW_OPTIONS": "Menampilkan pilihan",
+    "UserForms.ADDING_OPTION": "Menambahkan Pilihan",
+    "UserForms.ADDED_OPTION": "Menambahkan Pilihan",
+    "UserForms.ERROR_CREATING_OPTION": "Error creating option",
+    "UserForms.REMOVED_OPTION": "Removed option",
+    "UserForms.ADDING_RULE": "Menambahkan Aturan",
+    "GRIDFIELD.ERRORINTRANSACTION": "An error occured while fetching data from the server\n Please try again later."
+}
