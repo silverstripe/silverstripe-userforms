046b2a2a1161dad0760aa8329151fabc1475f09c
David Craig <david@silverstripe.com>
2015-07-13 10:05:14 +1200

    Add option for displaying error messages at the top of the form

--- thirdparty/jquery-validate/localization/methods_fi.js
+++ thirdparty/jquery-validate/localization/methods_fi.js
@@ -8,14 +8,14 @@
 
 /*
  * Localized default methods for the jQuery validation plugin.
- * Locale: ES_CL
+ * Locale: FI
  */
 $.extend($.validator.methods, {
 	date: function(value, element) {
-		return this.optional(element) || /^\d\d?\-\d\d?\-\d\d\d?\d?$/.test(value);
+		return this.optional(element) || /^\d{1,2}\.\d{1,2}\.\d{4}$/.test(value);
 	},
 	number: function(value, element) {
-		return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+		return this.optional(element) || /^-?(?:\d+)(?:,\d+)?$/.test(value);
 	}
 });
 
