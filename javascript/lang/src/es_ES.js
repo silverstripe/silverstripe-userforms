72c89dc38585dc86e7cd7909d69736209275b2c0
Damian Mooyman <dmooyman@MacBook-Pro.local>
2015-08-20 12:47:37 +1200

    Update translations

--- javascript/lang/src/es_ES.js
+++ javascript/lang/src/es_ES.js
@@ -1,14 +1,14 @@
 {
-    "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "All submissions will be permanently removed. Continue?",
-    "UserForms.ERROR_CREATING_FIELD": "Error creating field",
-    "UserForms.ADDING_FIELD": "Adding new field",
-    "UserForms.ADDED_FIELD": "Added new field",
-    "UserForms.HIDE_OPTIONS": "Hide options",
-    "UserForms.SHOW_OPTIONS": "Show options",
-    "UserForms.ADDING_OPTION": "Adding option",
-    "UserForms.ADDED_OPTION": "Added option",
-    "UserForms.ERROR_CREATING_OPTION": "Error creating option",
-    "UserForms.REMOVED_OPTION": "Removed option",
-    "UserForms.ADDING_RULE": "Adding rule",
-    "GRIDFIELD.ERRORINTRANSACTION": "An error occured while fetching data from the server\n Please try again later."
+    "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "Se eliminarán todos los envíos. ¿Continuar?",
+    "UserForms.ERROR_CREATING_FIELD": "Error al crear campo",
+    "UserForms.ADDING_FIELD": "Agregar un nuevo campo",
+    "UserForms.ADDED_FIELD": "Nuevo campo agregado",
+    "UserForms.HIDE_OPTIONS": "Ocultar opciones",
+    "UserForms.SHOW_OPTIONS": "Mostrar opciones",
+    "UserForms.ADDING_OPTION": "Agregar opción",
+    "UserForms.ADDED_OPTION": "Opción agregada",
+    "UserForms.ERROR_CREATING_OPTION": "Error al crear opción",
+    "UserForms.REMOVED_OPTION": "Opción eliminada",
+    "UserForms.ADDING_RULE": "Agregar regla",
+    "GRIDFIELD.ERRORINTRANSACTION": "Ocurrió un error mientras se obtenían datos desde el servidor\n Por favor, más tarde, inténtelo nuevamente."
 }
