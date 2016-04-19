cb605d563390a685a0ccdb0d10e6ec203a82fe3b
micmania1 <mstrong@silverstripe.org>
2016-04-19 22:40:37 +0000

    NEW Added option to allow empty values

--- code/model/editableformfields/EditableOption.php
+++ code/model/editableformfields/EditableOption.php
@@ -32,6 +32,26 @@ class EditableOption extends DataObject {
 		'Default'
 	);
 
+    protected static $allow_empty_values = false;
+
+    /**
+     * Returns whether to allow empty values or not.
+     *
+     * @return boolean
+     */
+    public static function allow_empty_values() {
+        return (bool) self::$allow_empty_values;
+    }
+
+    /**
+     * Set whether to allow empty values.
+     *
+     * @param boolean $allow
+     */
+    public static function set_allow_empty_values($allow) {
+        self::$allow_empty_values = (bool) $allow;
+    }
+
 	/**
 	 * @param Member $member
 	 *
@@ -114,5 +134,20 @@ class EditableOption extends DataObject {
      */
     public function canUnpublish($member = null) {
         return $this->canDelete($member);
+    }
+
+    /**
+     * Fetches a value for $this->Value. If empty values are not allowed,
+     * then this will return the title in the case of an empty value.
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        $value = $this->getField('Value');
+        if(empty($value) && !self::allow_empty_values()) {
+            return $this->Title;
+        }
+        return $value;
     }
 }
