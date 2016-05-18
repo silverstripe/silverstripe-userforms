42bf3e0a569444a50dfbb0aac4124ba24a3c1ea9
Damian Mooyman <damian@silverstripe.com>
2016-05-18 16:58:44 +1200

    Update translations

---
+++ javascript/lang/src/sk.js
@@ -0,0 +1,14 @@
+{
+    "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "Všetky podané formuláre budú permanentne odstránené. Pokračovať?",
+    "UserForms.ERROR_CREATING_FIELD": "Nastala chyba pri vytváraní poľa",
+    "UserForms.ADDING_FIELD": "Pridávanie nového políčka",
+    "UserForms.ADDED_FIELD": "Nové políčko pridané",
+    "UserForms.HIDE_OPTIONS": "Skryť možnosti",
+    "UserForms.SHOW_OPTIONS": "Zobraziť možnosti",
+    "UserForms.ADDING_OPTION": "Pridávanie voľby/možnosti",
+    "UserForms.ADDED_OPTION": "Voľba/možnosť pridaná",
+    "UserForms.ERROR_CREATING_OPTION": "Nastala chyba pri vytváraní voľby/možnosti",
+    "UserForms.REMOVED_OPTION": "Voľba/možnosť odstránená",
+    "UserForms.ADDING_RULE": "Pravidlo priadné",
+    "GRIDFIELD.ERRORINTRANSACTION": "Nastala chyba pri získavaní dát zo servera.\nProsím, skúste to neskôr."
+}
