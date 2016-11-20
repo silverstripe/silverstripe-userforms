c3ba8f42e1edb67b1a97dc4d704a9b8af983d35d
Eduard Briem <ed@hothouse.co.nz>
2016-11-21 10:48:10 +1300

    using forTemplate() instead of (string) casting fixes unit test

--- code/model/editableformfields/EditableLiteralField.php
+++ code/model/editableformfields/EditableLiteralField.php
@@ -145,7 +145,7 @@ class EditableLiteralField extends EditableFormField
                 Convert::raw2htmlname($this->Name),
                 Convert::raw2att($classes),
                 $label,
-                (string)$this->dbObject('Content')
+                $this->dbObject('Content')->forTemplate()
             )
         );
 
