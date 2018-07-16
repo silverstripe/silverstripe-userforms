d327f0d53f229998300daf825e6399bfe790bb2b
Andreas Gerhards <andreasg@intercitygroup.co.nz>
2018-07-16 15:44:08 +1200

    NEW Add SS 3.7 and PHP 7.2 compatability

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
 
 if (!defined('USERFORMS_DIR')) {
     define('USERFORMS_DIR', basename(__DIR__));
