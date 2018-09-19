8ab4561dc64daca62500c097a239c896083bebf9
Hendrik Schaper <hendrik@schaper-software.de>
2018-09-19 12:16:57 +0200

    PHP 7.2 compatibility
    Use SS_Object instead of Object

--- _config.php
+++ _config.php
@@ -3,5 +3,10 @@
 if (!defined('USERFORMS_DIR')) {
     define('USERFORMS_DIR', basename(__DIR__));
 }
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
 
 Deprecation::notification_version('3.0', 'userforms');
