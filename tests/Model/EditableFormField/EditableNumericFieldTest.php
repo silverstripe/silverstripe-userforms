f235c8642cc4f6deb8c706972993bb14b2c6372b
Robbie Averill <robbie@silverstripe.com>
2019-01-16 21:59:02 +0100

    Add unit tests for EditableNumericField::validate()

--- tests/Model/EditableFormField/EditableNumericFieldTest.php
+++ tests/Model/EditableFormField/EditableNumericFieldTest.php
@@ -14,4 +14,27 @@ class EditableNumericFieldTest extends SapphireTest
         $field->Name = 'EditableFormField_123456';
         $this->assertEmpty($field->getFormField()->Title());
     }
+
+    public function testValidateAddsErrorWhenMinValueIsGreaterThanMaxValue()
+    {
+        /** @var EditableNumericField $field */
+        $field = EditableNumericField::create();
+        $field->MinValue = 10;
+        $field->MaxValue = 5;
+
+        $result = $field->validate();
+        $this->assertFalse($result->isValid(), 'Validation should fail when min is greater than max');
+        $this->assertContains('Minimum length should be less than the maximum length', $result->serialize());
+    }
+
+    public function testValidate()
+    {
+        /** @var EditableNumericField $field */
+        $field = EditableNumericField::create();
+        $field->MinValue = 5;
+        $field->MaxValue = 10;
+
+        $result = $field->validate();
+        $this->assertTrue($result->isValid());
+    }
 }
