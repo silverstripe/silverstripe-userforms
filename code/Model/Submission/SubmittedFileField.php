beaf90120865b9f5da7d3e46631eb881b96f1a75
Will Rossiter <will@fullscreen.io>
2021-03-31 15:26:38 +1300

    FIX When deleting form submission, delete linked file

--- code/Model/Submission/SubmittedFileField.php
+++ code/Model/Submission/SubmittedFileField.php
@@ -4,6 +4,7 @@ namespace SilverStripe\UserForms\Model\Submission;
 
 use SilverStripe\Assets\File;
 use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\Versioned\Versioned;
 
 /**
  * A file uploaded on a {@link UserDefinedForm} and attached to a single
@@ -21,6 +22,14 @@ class SubmittedFileField extends SubmittedFormField
 
     private static $table_name = 'SubmittedFileField';
 
+    private static $owns = [
+        'UploadedFile'
+    ];
+
+    private static $cascade_deletes = [
+        'UploadedFile'
+    ];
+
     /**
      * Return the value of this field for inclusion into things such as
      * reports.
@@ -31,7 +40,7 @@ class SubmittedFileField extends SubmittedFormField
     {
         $name = $this->getFileName();
         $link = $this->getLink();
-        $title = _t(__CLASS__.'.DOWNLOADFILE', 'Download File');
+        $title = _t(__CLASS__ . '.DOWNLOADFILE', 'Download File');
 
         if ($link) {
             return DBField::create_field('HTMLText', sprintf(
@@ -72,14 +81,14 @@ class SubmittedFileField extends SubmittedFormField
     /**
      * As uploaded files are stored in draft by default, this retrieves the
      * uploaded file from draft mode rather than using the current stage.
-     * 
+     *
      * @return File
      */
-    public function getUploadedFileFromDraft(): ?File
+    public function getUploadedFileFromDraft()
     {
         $fileId = $this->UploadedFileID;
 
-        return Versioned::withVersionedMode(function() use ($fileId) {
+        return Versioned::withVersionedMode(function () use ($fileId) {
             Versioned::set_stage(Versioned::DRAFT);
 
             return File::get()->byID($fileId);
