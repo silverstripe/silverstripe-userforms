f31d031fd6d361e0d74bee3fd4b4becd8c1f2133
Steve Boyd <emteknetnz@gmail.com>
2024-12-04 13:04:02 +1300

    ENH Remove duplicate calls to form field validate

--- code/Form/UserFormsRequiredFields.php
+++ code/Form/UserFormsRequiredFields.php
@@ -35,12 +35,6 @@ class UserFormsRequiredFields extends RequiredFields
         $valid = true;
         $fields = $this->form->Fields();
 
-        foreach ($fields as $field) {
-            $result = $field->validate();
-            $valid = $result->isValid() && $valid;
-            $this->result->combineAnd($result);
-        }
-
         if (empty($this->required)) {
             return $valid;
         }
