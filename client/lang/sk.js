5afbbb50e2754178ab64dbd87bd3c4979565e63b
Guy Sartorelli <guy.sartorelli@gmail.com>
2025-05-19 12:14:45 +1200

    TLN Update translations

--- client/lang/sk.js
+++ client/lang/sk.js
@@ -13,6 +13,7 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
     "UserForms.ADDING_OPTION": "Pridávanie voľby/možnosti",
     "UserForms.ADDING_RULE": "Pravidlo priadné",
     "UserForms.CONFIRM_DELETE_ALL_SUBMISSIONS": "Všetky podané formuláre budú permanentne odstránené. Pokračovať?",
+    "UserForms.ERROR_CONTAINER_HEADER": "Please correct the following errors and try again:",
     "UserForms.ERROR_CREATING_FIELD": "Nastala chyba pri vytváraní poľa",
     "UserForms.ERROR_CREATING_OPTION": "Nastala chyba pri vytváraní voľby/možnosti",
     "UserForms.HIDE_OPTIONS": "Skryť možnosti",
